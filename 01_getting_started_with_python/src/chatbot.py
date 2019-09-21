e90407d08790e317b4c4c1896e77f5f9bd62211b
eotp <krois@zedat.fu-berlin.de>
2019-09-21 10:07:05 +0200

    updates to section one

--- 01_getting_started_with_python/src/chatbot.py
+++ 01_getting_started_with_python/src/chatbot.py
@@ -3,17 +3,17 @@ print("Hi, my name is Vincent.")
 time.sleep(1)
 print("What´s your name?")
 user = input()
-if user == "Joachim":
+if user.lower == "joachim":
     print("Good morning my creator!")
     print('')
-elif user == 'Mieke':
+elif user.lower() == 'mieke':
     print("Oh, what a pleasure! It feels great to have the Hello World Academy in the house.")
     time.sleep(1)
     print("Is Joachim somewhere around too?")
     answer = input()
-    if answer == 'yes':
+    if answer.lower() == 'yes':
         print('Great, a strong team!')
-    elif answer == 'no':
+    elif answer.lower == 'no':
         print('OK, if you see him make sure he knows that we miss him.')
     else:
         print("Well, did not get that, but thats's fine. See you buddy!")
