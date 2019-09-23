3ea7841396f65bcfc0136bd2a0c1bdabf7abb1cc
eotp <krois@zedat.fu-berlin.de>
2019-09-23 21:12:16 +0200

    refactoring using urls as src

--- 04_introduction_to_html_css_js/html/scripts/image_switcher.js
+++ 04_introduction_to_html_css_js/html/scripts/image_switcher.js
@@ -4,9 +4,9 @@ var myImage = document.querySelector('img');
 
 myImage.onclick = function() {
   var mySrc = myImage.getAttribute('src');
-  if(mySrc === 'images/Beiersdorf-New-Headquarters.png') {
-    myImage.setAttribute ('src','images/BDF_Logo.png');
+  if(mySrc === 'https://hello-world.academy/wp-content/uploads/2019/09/Beiersdorf-New-Headquarters.png') {
+    myImage.setAttribute ('src','https://hello-world.academy/wp-content/uploads/2019/09/BDF_Logo.png');
   } else {
-    myImage.setAttribute ('src','images/Beiersdorf-New-Headquarters.png');
+    myImage.setAttribute ('src','https://hello-world.academy/wp-content/uploads/2019/09/Beiersdorf-New-Headquarters.png');
   }
 }
